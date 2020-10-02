6f0d376a0b74a39d1081bb66cde44bc138dec45c
Peter Calabrese <peter@thirtyfour.tech>
2020-10-02 19:44:56 -0400

    added splashscreen

--- app/src/main/java/tech/thirtyfour/login_r01861244_system/MainActivity.java
+++ app/src/main/java/tech/thirtyfour/login_r01861244_system/MainActivity.java
@@ -2,7 +2,9 @@ package tech.thirtyfour.login_r01861244_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
